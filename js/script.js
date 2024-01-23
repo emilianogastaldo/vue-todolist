f0a33be9ba36a7b82891a61b70ad624336702f3b
emilianogastaldo <emiliano0303@gmail.com>
2024-01-23 18:52:34 +0100

    added filteredtasks computed

--- js/script.js
+++ js/script.js
@@ -4,15 +4,23 @@ const { createApp } = Vue;
 const app = createApp({
     name: 'ToDo List',
     data: () => ({
-        prova: 'prova VderrerE',
+        prova: 'prova Vue',
         tasks: [
             { id: 1, done: false, text: 'Fare la spesa' },
             { id: 2, done: true, text: 'Lavare i capelli' },
             { id: 3, done: false, text: 'Comprare una marca da bollo' },
             { id: 4, done: false, text: 'Aggiornare il PC' }
         ],
-        newTaskText: ''
+        newTaskText: '',
+        searchText: ''
     }),
+    computed: {
+        filteredTasks() {
+            const search = this.searchText.toLowerCase()
+            const filteredArray = this.tasks.filter(task => task.text.toLowerCase().includes(search));
+            return filteredArray;
+        },
+    },
     methods: {
         deleteTask(id) {
             this.tasks = this.tasks.filter(task => task.id !== id);
@@ -32,6 +40,9 @@ const app = createApp({
             this.tasks.push(task);
             this.newTaskText = '';
         },
+        searchTask(text) {
+            this.task
+        }
     }
 });
 app.mount('#root');
