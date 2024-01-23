495366f6ac210e6feca55ba7d098980fbca9d166
emilianogastaldo <emiliano0303@gmail.com>
2024-01-23 16:25:59 +0100

    added delete task method

--- js/script.js
+++ js/script.js
@@ -11,7 +11,15 @@ const app = createApp({
             { id: 3, done: false, text: 'Comprare una marca da bollo' },
             { id: 4, done: false, text: 'Aggiornare il PC' }
         ]
-    })
-
+    }),
+    methods: {
+        deleteTask(id) {
+            const filteredTasks = this.tasks.filter(task => {
+                if (task.id !== id) return true;
+                else return false;
+            })
+            this.tasks = filteredTasks;
+        }
+    }
 });
 app.mount('#root');
