1af60269a3ff18ea85fb4fa0b9c5e894fb2c5149
emilianogastaldo <emiliano0303@gmail.com>
2024-01-23 16:03:43 +0100

    created app and linked to the DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log(Vue);
+
+const { createApp } = Vue;
+const app = createApp({
+    name: 'ToDo List',
+    data: () => ({
+        prova: 'prova VderrerE'
+    })
+
+});
+app.mount('#root');
